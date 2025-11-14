213bd468699be9fdc40a4fb2226477e351809271
DhruvParmar051 <dhruvparmar051@outlook.com>
2025-11-14 14:30:08 +0530

    pipeline mistake cleaning

--- main.py
+++ main.py
@@ -22,9 +22,7 @@ def main():
 
     BASE_DIR = os.getcwd()
 
-    # -------------------------------
     # PATH CONFIGURATION
-    # -------------------------------
     RAW_DATA_PATH = os.path.join(BASE_DIR, "data", "raw_data", "data.gzip")
 
     CLEANED_DIR = os.path.join(BASE_DIR, "data", "cleaned_data")
@@ -43,27 +41,20 @@ def main():
     os.makedirs(PROCESSED_DIR, exist_ok=True)
     os.makedirs(MODEL_DIR, exist_ok=True)
 
-    # -------------------------------
-    # STEP 1: CLEANING
-    # -------------------------------
+
+    # STEP 1: cleaning
     logger.info("STEP 1: Running Data Cleaning Pipeline")
     clean_data_pipeline(RAW_DATA_PATH, CLEANED_DIR)
 
-    # -------------------------------
-    # STEP 2: FEATURE ENGINEERING
-    # -------------------------------
+    # STEP 2: feature engineering
     logger.info("STEP 2: Running Feature Engineering Pipeline")
     feature_engineering_pipeline(CLEANED_OUTPUT, FEATURE_DIR)
 
-    # -------------------------------
-    # STEP 3: PREPROCESSING
-    # -------------------------------
+    # STEP 3: preprocessing
     logger.info("STEP 3: Running Data Preprocessing Pipeline")
     data_preprocessing_pipeline(FEATURE_OUTPUT, PROCESSED_DIR)
 
-    # -------------------------------
-    # STEP 4: MODEL TRAIN + TUNING
-    # -------------------------------
+    # STEP 4: Model Training + Tuning
     logger.info("STEP 4: Running Model Tuning Pipeline")
     model_tuning_pipeline(PROCESSED_DIR, MODEL_DIR)
 
